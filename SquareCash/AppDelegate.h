abf6f9aba8c536a497cc6420864f7eddf84d63c8
Hailstorm350 <Hailstorm350@gmail.com>
2013-05-27 08:14:55 -0700

    Initial Commit

---
+++ SquareCash/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SquareCash
+//
+//  Created by Kenneth Wigginton on 5/27/13.
+//  Copyright (c) 2013 Ken Wigginton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SquareCash/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SquareCash
+//
+//  Created by Kenneth Wigginton on 5/27/13.
+//  Copyright (c) 2013 Ken Wigginton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
